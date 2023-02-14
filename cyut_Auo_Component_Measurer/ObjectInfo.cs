278f29cb7ea9ee66174bcf01b3a8bf48828ea74c
Albus <b123963@yahoo.com.tw>
2023-02-14 09:26:46 +0800

    build ObjectInfo

---
+++ cyut_Auo_Component_Measurer/ObjectInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cyut_Auo_Component_Measurer
+{
+    public class ObjectInfo
+    {
+        public float CenterX { get; set; }
+
+        public float CenterY { get; set; }
+
+        public float width { get; set; }
+        public float widthError { get; set; }
+        public float widthStd { get; set; }
+
+        public float height { get; set; }
+        public float heightError { get; set; }
+        public float heightStd { get; set; }
+
+
+        public string ShapeName { get; set; }
+
+        public int CheckResult { get; set; }
+
+        public uint ElementIndex { get; set; }
+    }
+}
